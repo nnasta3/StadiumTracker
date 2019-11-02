b4f465aa25a88c5d77fbe1724493763c5a70aeae
strauscon <johnstrauser@gmail.com>
2019-11-02 18:22:31 -0400

    db stuff

--- Android/app/src/main/java/com/example/stadiumtracker/LoginActivity.java
+++ Android/app/src/main/java/com/example/stadiumtracker/LoginActivity.java
@@ -1,21 +1,21 @@
 package com.example.stadiumtracker;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText usernameField;
     EditText passwordField;
-    private ProgressDialog dialog;
 
-    private static String url_login = R.string.base_url + "login_activity"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +24,10 @@ public class LoginActivity extends AppCompatActivity {
 
          usernameField = (EditText) findViewById(R.id.username_login);
          passwordField = (EditText) findViewById(R.id.password_login);
+
     }
 
     public void loginHandler(View v){
-        //TODO: query datatbase for usernames and emails where password = text field
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
 
@@ -39,33 +39,31 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Please Enter A Password",Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(getApplicationContext(),"Query Database would occur here",Toast.LENGTH_SHORT).show();
+
+        try{
+            ResultSet set = new dbConnect().execute("select username from users").get();
+            int valid = 0;
+            while(set.next()){
+                if(set.getString(1).equals(username)){
+                    Toast.makeText(getApplicationContext(),"valid login",Toast.LENGTH_SHORT).show();
+                    //TODO: Replace the valid thing with an intent switch
+                    return;
+                }
+            }
+            if(valid == 0){
+                Toast.makeText(getApplicationContext(),"This would be an invalid login",Toast.LENGTH_SHORT).show();
+            }
+        }catch (SQLException e){
+            Log.w("Error","" + e);
+            Toast.makeText(getApplicationContext(),"This would be an invalid login error",Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Log.w("Error","" + e);
+        }
+
     }
 
     public void registerHandler(View v){
-        //TODO: send to signup activity
-        //Toast.makeText(getApplicationContext(),"Send to register page would occur here",Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this,SignUpActivity.class);
         startActivity(intent);
-    }
-
-    class loginHandlerClass extends AsyncTask<String, String, String>{
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            dialog = new ProgressDialog(LoginActivity.this);
-            dialog.setMessage("Attempting Login.");
-            dialog.setIndeterminate(false);
-            dialog.setCancelable(false);
-            dialog.show();
-        }
-        @Override
-        protected String doInBackground(String... strings) {
-            return null;
-        }
-        protected void onPostExecute(String idkWhatToUseThisFor){
-            dialog.dismiss();
-        }
     }
 }
