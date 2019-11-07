ec1287a055276a26b8b023e3d12f36eb7c91d4ef
John Strauser <johnstrauser@gmail.com>
2019-11-06 21:29:32 -0500

    record stadium spinner stuff added, incomplete and currently crashes due to arrayadapter looking for string and getting stadium.

--- Android/app/src/main/java/com/example/stadiumtracker/RecordActivity.java
+++ Android/app/src/main/java/com/example/stadiumtracker/RecordActivity.java
@@ -2,13 +2,23 @@ package com.example.stadiumtracker;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.stadiumtracker.data.Stadium;
 import com.example.stadiumtracker.data.User;
@@ -25,6 +35,7 @@ public class RecordActivity extends AppCompatActivity {
 
     List<String> leagueList;
     List<Stadium> stadiumList;
+    Stadium selectedStadium;
 
     Calendar calendar;
     String dateString;
@@ -43,15 +54,67 @@ public class RecordActivity extends AppCompatActivity {
         roadScore = findViewById(R.id.record_road_score);
         stadiumSpinner = findViewById(R.id.record_stadium_spinner);
         leagueSpinner = findViewById(R.id.record_league_spinner);
+        selectedStadium = null;
+
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         calendar = Calendar.getInstance();
-        dateString = calendar.get(Calendar.DAY_OF_MONTH)+"/"+calendar.get(Calendar.MONTH)+"/"+calendar.get(Calendar.YEAR);
+        dateString = calendar.get(Calendar.DAY_OF_MONTH)+"/"+(calendar.get(Calendar.MONTH)+1)+"/"+calendar.get(Calendar.YEAR);
         dateBox.setText(dateString);
 
-        //TODO: Stadium Spinner list, start with hint "stadium"
+        // Stadium Spinner list, start with hint "stadium"
         stadiumList = new ArrayList<>();
-        //stadiumList.add("Stadium");
+        stadiumList.add(new Stadium());
         addStadiums();
+
+        stadiumList.add(new Stadium(0,"example","example","example"));
+        //TODO: Stadium spinner setup
+        ArrayAdapter<Stadium> stadiumArrayAdapter = new ArrayAdapter<Stadium>(this, R.layout.spinner_item,stadiumList){
+          @Override
+          public boolean isEnabled(int position){
+              if(position == 0){
+                  return false;
+              }else{
+                  return true;
+              }
+          }
+          @Override
+          public  View getDropDownView(int position, View convertView, ViewGroup parent){
+              View view = super.getDropDownView(position, convertView, parent);
+              TextView textView = (TextView) view;
+              if(position == 0){
+                   textView.setTextColor(Color.GRAY);
+              }else{
+                  textView.setTextColor(Color.BLACK);
+              }
+              return view;
+          }
+        };
+        stadiumArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        stadiumSpinner.setAdapter(stadiumArrayAdapter);
+
+        stadiumSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                    selectedStadium = stadiumList.get(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //TODO: League Spinner list, start with hint "league"
         leagueList = new ArrayList<>();
@@ -59,6 +122,27 @@ public class RecordActivity extends AppCompatActivity {
         addLeagues();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.record_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                Intent intent = new Intent(this,LoginActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_gps:
+                //TODO: perform gps search for closest stadium
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
     private void addStadiums(){
         //TODO: query database for all stadiums
 
@@ -95,7 +179,7 @@ public class RecordActivity extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-            dateString = dayOfMonth+"/"+month+"/"+year;
+            dateString = dayOfMonth+"/"+(month+1)+"/"+year;
             dateBox.setText(dateString);
         }
     };
--- Android/app/src/main/java/com/example/stadiumtracker/data/Stadium.java
+++ Android/app/src/main/java/com/example/stadiumtracker/data/Stadium.java
@@ -1,4 +1,94 @@
 package com.example.stadiumtracker.data;
 
-public class Stadium {
+public class Stadium implements java.io.Serializable{
+    private int stadiumID;
+    private String name;
+    private String city;
+    private String country;
+    private double gpsLat;
+    private double gpsLong;
+
+    //This constructor should only really be used for the first entry in a dropdown
+    public Stadium(){
+        stadiumID = -1;
+        name = "Stadium";
+        city = "";
+        country = "";
+        gpsLat = 0;
+        gpsLong = 0;
+    }
+
+    public Stadium(int stadiumID, String name, String city, String country){
+        this.stadiumID=stadiumID;
+        this.name=name;
+        this.city=city;
+        this.country=country;
+        this.gpsLat=0;
+        this.gpsLong=0;
+    }
+
+    public Stadium(int stadiumID, String name, String city, String country, double gpsLat, double gpsLong){
+        this.stadiumID=stadiumID;
+        this.name=name;
+        this.city=city;
+        this.country=country;
+        this.gpsLat=gpsLat;
+        this.gpsLong=gpsLong;
+    }
+
+    public int getStadiumID() {
+        return stadiumID;
+    }
+
+    public void setStadiumID(int stadiumID) {
+        this.stadiumID = stadiumID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getGpsLat() {
+        return gpsLat;
+    }
+
+    public void setGpsLat(double gpsLat) {
+        this.gpsLat = gpsLat;
+    }
+
+    public double getGpsLong() {
+        return gpsLong;
+    }
+
+    public void setGpsLong(double gpsLong) {
+        this.gpsLong = gpsLong;
+    }
+
+    public String toString(){
+        if(stadiumID != -1){
+            return name+", "+city+", "+country;
+        }else{
+            return name;
+        }
+    }
 }
