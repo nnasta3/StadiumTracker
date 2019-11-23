9aea1bdef957e7885b05323c8edd794c7a35543f
Nastaman <nnasta3@gmail.com>
2019-11-22 20:04:23 -0500

    Update DatabaseTests.java
    half of tests

--- Android/app/src/test/java/com/example/stadiumtracker/DatabaseTests.java
+++ Android/app/src/test/java/com/example/stadiumtracker/DatabaseTests.java
@@ -4,10 +4,18 @@ import android.content.Context;
 
 import androidx.test.core.app.ApplicationProvider;
 
+import com.example.stadiumtracker.data.Stadium;
 import com.example.stadiumtracker.database.loginQuery;
-
+import com.example.stadiumtracker.database.insertUser;
+import com.example.stadiumtracker.database.eventsQuery;
+import com.example.stadiumtracker.database.eventCreate;
+import com.example.stadiumtracker.database.allStadiums;
+import com.example.stadiumtracker.database.allLeagues;
+import com.example.stadiumtracker.data.Event;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.*;
@@ -20,24 +28,68 @@ public class DatabaseTests {
     //TODO: query for allLeagues
     @Test
     public void testAllLeagues(){
+        List<String> list = new ArrayList<>();
+        try {
+            list = new allLeagues(context).execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        int length = 3;
+        assertTrue(length == list.size());
     }
 
     //TODO: query for allStadiums
     @Test
     public void testAllStadiums(){
 
+        List<Stadium> list = new ArrayList<>();
+        try {
+            list = new allStadiums(context).execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int length = 14;
+        assertTrue(length == list.size());
     }
 
     //TODO: query for eventCreate
     @Test
     public void testEventCreate(){
 
+        int newEvent = 0;
+        try {
+            newEvent = new eventCreate(context).execute("12","2019-10-15","17","19","1","2","MLB").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int expected = 1;
+
+        assertEquals(expected,newEvent);
     }
 
     //TODO: query for eventsQuery
     @Test
     public void testEventsQuery(){
+
+        Event newEvent = null;
+        try {
+            newEvent = new eventsQuery(context).execute("7","2019-10-11","16","19","1","2","MLB").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertNotNull(newEvent);
 
     }
 
@@ -45,6 +97,16 @@ public class DatabaseTests {
     @Test
     public void testInsertUser(){
 
+        boolean userID = false;
+        try {
+            userID = new insertUser(context).execute("joe","password").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(userID);
     }
 
     //test for loginQuery
